d30fb135835f705b9fee3f46304320b97e749851
Giiid <gideonfummey1@gmail.com>
2023-12-25 21:13:28 +0000

    Bookmarks blueprint

--- flask_api/__init__.py
+++ flask_api/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask_api.config import Config
 from flask_api.users.routes import users
+from flask_api.bookmarks.routes import bookmarks
 from flask_api.utils import bcrypt, db
 from flask_jwt_extended import JWTManager
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
     
     app.register_blueprint(users)
+    app.register_blueprint(bookmarks)
 
     JWTManager(app)
-    
+
     db.init_app(app)
     bcrypt.init_app(app)
     return app
